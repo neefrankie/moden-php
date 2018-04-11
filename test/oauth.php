02f2b53daf59b36391e4657a87485152e41b9117
weiguo.ni <neefrankie@gmail.com>
2018-04-11 11:42:32 +0800

    get rid of wxpay

--- test/oauth.php
+++
@@ -1,5 +0,0 @@
-<?php
-require dirname(__FILE__) . "./../WxpayAPI_php_v3.0.1/example/WxPay.JsApiPay.php";
-
-$tools = new JsApiPay();
-$openId = $tools->GetOpenid();
--- test/wepay-xml.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require dirname(__FILE__) .  './../WxpayAPI_php_v3.0.1/lib/WxPay.Data.php';
-
-$order = new WxPayUnifiedOrder();
-$order->SetAppid("abcdedfj");
-$order->SetTotal_fee(100);
-var_dump($order->ToXml());
